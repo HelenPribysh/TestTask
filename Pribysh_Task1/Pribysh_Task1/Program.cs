9648b6987d7a98b47da0b3aa83b16db32115bcfd
he11en_ <helenpribysh@gmail.com>
2020-12-19 01:01:18 +0300

    Task1

---
+++ Pribysh_Task1/Pribysh_Task1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Pribysh_Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string number = Console.ReadLine();
+            if (Convert.ToInt32(number) > 7)
+            {
+                Console.WriteLine("Привет");
+            }
+
+            else
+            {
+                Console.WriteLine("Число меньше или равно 7");
+            }
+        }
+    }
+}
