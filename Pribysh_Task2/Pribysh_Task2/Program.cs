0a2af4f1bfd5b01c222919dab2ef2ea51c43074d
he11en_ <helenpribysh@gmail.com>
2020-12-19 01:28:43 +0300

    Task2

---
+++ Pribysh_Task2/Pribysh_Task2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Pribysh_Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Введите Имя:");
+                string name = Console.ReadLine();
+                if (string.Equals(name.ToLower(), "вячеслав"))
+                {
+                    Console.WriteLine("Привет, Вячеслав");
+                }
+
+                else
+                {
+                    Console.WriteLine("Нет такого имени");
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
